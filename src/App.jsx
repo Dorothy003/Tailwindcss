656b6cca86014d486ee1703309c683706a8e81d6
Dorothy003 <gogoidorothy198@gmail.com>
2024-06-21 10:27:12 +0530

    React-Vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1 className ='text-xl text-bold text-red-500'>Hello tailwindcss with react </h1>
+    </>
+  )
+}
+
+export default App
